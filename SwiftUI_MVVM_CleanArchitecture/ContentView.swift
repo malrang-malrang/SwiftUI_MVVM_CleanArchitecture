a88db9ffe6297e87ba4bb91bfb27e555eb46d862
malrang <ruehd7924@gmail.com>
2023-11-18 16:37:48 +0900

    ✅[chore]: Project InitialSetting

---
+++ SwiftUI_MVVM_CleanArchitecture/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SwiftUI_MVVM_CleanArchitecture
+//
+//  Created by 김동욱 on 11/18/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SwiftUI_MVVM_CleanArchitecture/SwiftUI_MVVM_CleanArchitectureApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_MVVM_CleanArchitectureApp.swift
+//  SwiftUI_MVVM_CleanArchitecture
+//
+//  Created by 김동욱 on 11/18/23.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_MVVM_CleanArchitectureApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
