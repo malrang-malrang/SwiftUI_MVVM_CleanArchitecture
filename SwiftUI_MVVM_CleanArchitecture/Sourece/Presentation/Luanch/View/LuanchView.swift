31e90c9637a7b7e4189dccd912beb4b18d0492ac
malrang <ruehd7924@gmail.com>
2023-11-19 21:50:48 +0900

    ➕[add]: AppIcon 추가및 Malrang Image Asset 추가

---
+++ SwiftUI_MVVM_CleanArchitecture/Sourece/Presentation/Luanch/View/LuanchView.swift
@@ -0,0 +1,18 @@
+//
+//  LuanchView.swift
+//  SwiftUI_MVVM_CleanArchitecture
+//
+//  Created by 김동욱 on 11/19/23.
+//
+
+import SwiftUI
+
+struct LuanchView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    LuanchView()
+}
