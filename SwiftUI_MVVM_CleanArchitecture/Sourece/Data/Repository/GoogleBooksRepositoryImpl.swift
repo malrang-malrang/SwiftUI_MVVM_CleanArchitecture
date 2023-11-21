d1bdd214595bf55dbd99894b7b06cc819b442bc2
dw.kim <dw.kim@newploy.net>
2023-11-21 14:13:26 +0900

    ✅[chore]: GoogleBooksRepository return type Result -> async throws

--- SwiftUI_MVVM_CleanArchitecture/Sourece/Data/Repository/GoogleBooksRepositoryImpl.swift
+++ SwiftUI_MVVM_CleanArchitecture/Sourece/Data/Repository/GoogleBooksRepositoryImpl.swift
@@ -10,17 +10,17 @@ import Combine
 import Moya
 
 final class GoogleBooksRepositoryImpl: GoogleBooksRepository {
-  private let googleBooksProvider: NetworkProvider<GoogleBooksAPI>
+	private let googleBooksProvider: NetworkProvider<GoogleBooksAPI>
 
-  init(googleBooksProvider: NetworkProvider<GoogleBooksAPI> = NetworkProvider<GoogleBooksAPI>()) {
-    self.googleBooksProvider = googleBooksProvider
-  }
+	init(googleBooksProvider: NetworkProvider<GoogleBooksAPI> = NetworkProvider<GoogleBooksAPI>()) {
+		self.googleBooksProvider = googleBooksProvider
+	}
 
-  func getBookList(payload: BookSearchRequest) async throws -> Result<SearchResult, MoyaError> {
-    return await self.googleBooksProvider
-      .request(target: .getBookList(request: payload))
-      .map(\.data)
-      .decode(SearchResultDTO.self)
-      .map { $0.toDomain() }
-  }
+	func getBookList(payload: BookSearchRequest) async throws -> SearchResult {
+		return try await self.googleBooksProvider
+			.request(target: .getBookList(request: payload))
+			.map(\.data)
+			.decode(SearchResultDTO.self)
+			.toDomain()
+	}
 }
--- SwiftUI_MVVM_CleanArchitecture/Sourece/Utils/Extension/Result+Sugar.swift
+++ SwiftUI_MVVM_CleanArchitecture/Sourece/Utils/Extension/Result+Sugar.swift
@@ -9,22 +9,25 @@ import Foundation
 import Moya
 
 public extension Result where Success == (data: Data, response: URLResponse) {
-  func map<T>(_ keyPath: KeyPath<Success, T>) -> Result<T, Failure> {
-    switch self {
-    case .success(let response):
-      return .success(response[keyPath: keyPath])
-    case .failure(let error):
-      return .failure(error)
-    }
-  }
+	func map<T>(_ keyPath: KeyPath<Success, T>) -> Result<T, Failure> {
+		switch self {
+		case .success(let response):
+			return .success(response[keyPath: keyPath])
+		case .failure(let error):
+			return .failure(error)
+		}
+	}
 }
 
 public extension Result where Success == Data {
-  func decode<T: Decodable>(_ type: T.Type) -> Result<T, MoyaError> {
-    do {
-      return .success(try JSONDecoder().decode(type, from: try get()))
-    } catch {
-      return .failure(error as? MoyaError ?? .encodableMapping(error))
-    }
-  }
+	func decode<T: Decodable>(_ type: T.Type) throws -> T {
+		do {
+			switch self {
+			case .success(let data):
+				return try JSONDecoder().decode(type, from: data)
+			case .failure(let error):
+				throw error
+			}
+		}
+	}
 }
